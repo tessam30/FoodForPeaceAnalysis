f889a53cb1ad0880bf1a764215824c5135356ca0
Tim <tim.essam@gmail.com>
2016-07-05 17:00:33 -0400

    updates

--- R/01_plots.R
+++ R/01_plots.R
@@ -29,7 +29,7 @@ d$sortValue <- d$category
 d$sortValue <- factor(d$sortValue, levels = d$sortValue[order(d$totCatValue)])
 
 
-d_freq = d %>% group_by(category, moYear, totCount, monthlyCount) %>% 
+d_freq = d %>% group_by(category, moYear, totCount, monthlyCount, sortValue) %>% 
   summarise(count = n(), value = sum(povalue)/1000000, mean_value = mean(povalue)) %>% 
   arrange(category, -count)
 
@@ -43,40 +43,45 @@ p2 <- ggplot(d_freq, aes(moYear, category, fill = count)) +
   scale_x_date(date_breaks = "3 month", date_labels = "%b-%Y")+
   geom_vline(data=key.events, colour = "gray80", size = 1, linetype = "dashed",
              aes(xintercept=as.integer(date))) + 
+  labs(title = "Peas are the most commonly procured commodity") +
   theme_fivethirtyeight()
 
-
-
 p1 <- ggplot(d_freq, aes(moYear, y = count)) + geom_bar(stat = "identity") +
-  scale_x_date(date_breaks = "3 month", date_labels = "%b-%Y") +
+  #scale_x_date(date_breaks = "3 month", date_labels = "%b-%Y") +
   geom_vline(data=key.events, colour = "gray80", size = 1, linetype = "dashed",
-             aes(xintercept=as.integer(date))) + 
-  scale_y_continuous(breaks = c(0, 25, 50, 75, 100, 125)) +
+             aes(xintercept=as.integer(date))) +
+  scale_y_continuous(breaks = c(0, 25, 50, 75, 100, 125)) + 
+  labs(title = "The most procurements are executed in September", size = 4) +
   theme_fivethirtyeight()
 
 multiplot(p1, p2, cols =1)
 plot_grid(p1, p2, labels = c("A", "B"), align = "v", ncol = 1)  
-  
+
+
+# Focus on the unit cost average per commodity   
+p4 <- ggplot(d_freq, aes(moYear, sortValue, fill = value)) + 
   geom_tile(colour = "gray80", size = 0.25, stat = "identity") +
-  scale_fill_viridis(option="B") + 
-  geom_text(aes(y = category, x = moYear, label = round(count, 0)), size = 4, colour = "gray80") +
+  scale_fill_viridis(option="D") + 
   scale_x_date(date_breaks = "3 month", date_labels = "%b-%Y")+
   geom_vline(data=key.events, colour = "gray80", size = 1, linetype = "dashed",
              aes(xintercept=as.integer(date))) + 
-  theme_fivethirtyeight()
+  geom_text(aes(y = category, x = moYear, label = round(count, 1)), size = 2.5, colour = "gray80") +
+  theme_fivethirtyeight()   
 
-
-    
-ggplot(d_freq, aes(moYear, category, fill = value)) + 
-  geom_tile(colour = "gray80", size = 0.25, stat = "identity") +
-  scale_fill_viridis(option="D") + 
-  scale_x_date(date_breaks = "3 month", date_labels = "%b")+
+# Melt data down into 2 columns, one for value and one for date
+p3 <- d_freq %>% group_by(moYear) %>% 
+  summarise(totValue = sum(value)) %>% 
+  ggplot(aes(moYear, totValue)) + geom_bar(stat = "identity") +
+  #scale_x_date(date_breaks = "3 month", date_labels = "%b-%Y") +
+  xlab("")+
   geom_vline(data=key.events, colour = "gray80", size = 1, linetype = "dashed",
              aes(xintercept=as.integer(date))) + 
-  theme_fivethirtyeight()   
+  labs(title = "XXX") +theme(legend.position="right")+
+  theme_fivethirtyeight() 
+p3
     
-  
-  
+plotall <- plot_grid(p1, p2, p3, p4, labels = c("A", "B"), align = "v", ncol = 1, rel_heights=c(1,3,1,3)) 
+save_plot("frequencies.pdf", plotall, ncol = 1, useDingbats=FALSE)
 
 
  
